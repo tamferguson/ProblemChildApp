833c3ad8bb28521796be6a2dd5903bd6bb94541b
Tam Ferguson <tam.ferguson@gmail.com>
2012-04-25 16:40:26 -0500

    Mistakes fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   
   def index
-    mistakes = Mistake.all
+    @mistakes = Mistake.all
   end
 
   def show
-    @mistake = Mistake.find_by_id(params[id])
+    @mistake = Mistake.find_by_id(params[:id])
   end
 end
